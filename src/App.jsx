8a94dfa4db537f823a419bc5cd64d81d5bea83a6
Rishi358-art <rishinaik616@gmail.com>
2025-10-28 16:51:39 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1 className="text-blue-500">hello</h1>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
