e0457b55b7fdd7d0bfb57dd3232b5e02d347568e
Rishi Naik <rishinaik616@gmail.com>
2025-11-20 18:07:54 +0530

    size adjust

--- frontend/src/components/GptMovieSuggestions.jsx
+++ frontend/src/components/GptMovieSuggestions.jsx
@@ -6,7 +6,7 @@ const GptMovieSuggestions = () => {
   const {movieNames,movieResults}=useSelector(store=>store.gpt);
   if(movieNames===null || movieResults===null) return  ;
   return (
-    <div className="-mt-[155%] lg:-mt-[33%] relative  w-full  z-10 ">
+    <div className="-mt-[140%] lg:-mt-[33%] relative  w-full  z-10 ">
       {movieNames.map((movie,index)=><MovieList key={movie} title={movie} movies={movieResults[index]}/>)}
       
     </div>
